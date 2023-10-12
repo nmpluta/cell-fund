c3437bc5086ae060fdaf2aed1d34c3c1426cca08
Natalia Pluta <pluta.natalia.m@gmail.com>
2023-10-13 00:22:05 +0200

    add resolution of lesson3 ex. 1

--- lesson3/cellfund_less3_exer1/src/main.c
+++ lesson3/cellfund_less3_exer1/src/main.c
@@ -12,19 +12,23 @@
 #include <modem/lte_lc.h>
 
 /* STEP 3 - Include the header file for the socket API */
-
+#include <zephyr/net/socket.h>
 
 /* STEP 4 - Define the hostname and port for the echo server */
-
+#define SERVER_HOSTNAME "nordicecho.westeurope.cloudapp.azure.com"
+#define SERVER_PORT "2444"
 
 #define MESSAGE_SIZE 256
 #define MESSAGE_TO_SEND "Hello from nRF9160 SiP"
 #define SSTRLEN(s) (sizeof(s) - 1)
 
 /* STEP 5.1 - Declare the structure for the socket and server address */
+static int sock;
+struct sockaddr_storage server;
 
 
 /* STEP 5.2 - Declare the buffer for receiving from server */
+static char rx_buffer[MESSAGE_SIZE];
 
 K_SEM_DEFINE(lte_connected, 0, 1);
 
@@ -32,13 +36,46 @@ LOG_MODULE_REGISTER(Lesson3_Exercise1, LOG_LEVEL_INF);
 
 static int server_resolve(void)
 {
-	/* STEP 6.1 - Call getaddrinfo() to get the IP address of the echo server */
+	int err;
 
-	/* STEP 6.2 - Retrieve the relevant information from the result structure*/
+	/* STEP 6.1 - Call getaddrinfo() to get the IP address of the echo server */
+	struct addrinfo *result;
+	struct addrinfo hints = 
+	{
+		.ai_family = AF_INET,
+		.ai_socktype = SOCK_DGRAM,
+	};
+	
+	err = getaddrinfo(SERVER_HOSTNAME, SERVER_PORT, &hints, &result);
+	if (err != 0) 
+	{
+		LOG_ERR("getaddrinfo failed: %d\n", err);
+		return -1;
+	}
+
+	if (result == NULL) 
+	{
+		LOG_ERR("getaddrinfo failed: Address not found\n");
+		return -1;
+	}
+
+	/* STEP 6.2 - Retrieve the relevant information from the result structure */
+	struct sockaddr_in * p_server4 = (struct sockaddr_in *)&server;
+	p_server4->sin_family = hints.ai_family;
+	p_server4->sin_addr.s_addr = ((struct sockaddr_in *)result->ai_addr)->sin_addr.s_addr;
+	p_server4->sin_port = ((struct sockaddr_in *)result->ai_addr)->sin_port;
 
 	/* STEP 6.3 - Convert the address into a string and print it */
+	char ipv4_addr[NET_IPV4_ADDR_LEN];
+	inet_ntop(p_server4->sin_family, &p_server4->sin_addr, ipv4_addr, sizeof(ipv4_addr));
+
+	/* Print server_4 information. */
+	LOG_INF("Server family: %d", p_server4->sin_family);
+	LOG_INF("Server address: %s", ipv4_addr);
+	LOG_INF("Server port: %d", ntohs(p_server4->sin_port));
 
 	/* STEP 6.4 - Free the memory allocated for result */
+	freeaddrinfo(result);
 
 	return 0;
 }
@@ -47,8 +84,16 @@ static int server_connect(void)
 {
 	int err;
 	/* STEP 7 - Create a UDP socket */
+	static int sock;
+	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (sock < 0) 
+	{
+		LOG_ERR("Failed to create UDP socket: %d", errno);
+		return -1;
+	}
 
 	/* STEP 8 - Connect the socket to the server */
+	err = connect(sock, (struct sockaddr *) &server, NET_SOCKADDR_MAX_SIZE);
 
 	LOG_INF("Successfully connected to server");
 
@@ -106,11 +151,22 @@ static int modem_configure(void)
 
 static void button_handler(uint32_t button_state, uint32_t has_changed)
 {
-	switch (has_changed) {
-	case DK_BTN1_MSK:
-		/* STEP 9 - call send() when button 1 is pressed */
-
-	break;
+	int res;
+	switch (has_changed)
+	{
+		case DK_BTN1_MSK:
+			/* STEP 9 - call send() when button 1 is pressed */
+			res = send(sock, MESSAGE_TO_SEND, SSTRLEN(MESSAGE_TO_SEND), 0);
+			if (res < 0) 
+			{
+				LOG_ERR("Failed to send data to server: %d", errno);
+			}
+			else
+			{
+				LOG_INF("Successfully sent %d bytes to server", res);
+				LOG_INF("Message sent: %s", MESSAGE_TO_SEND);
+			}
+		break;
 	}
 }
 
@@ -148,7 +204,16 @@ int main(void)
 
 	while (1) {
 		/* STEP 10 - Call recv() to listen to received messages */
-
+		received = recv(sock, rx_buffer, sizeof(rx_buffer), 0);
+		if (received < 0) 
+		{
+			LOG_ERR("Failed to receive data from server: %d", errno);
+		}
+		else
+		{
+			LOG_INF("Received %d bytes from server", received);
+			LOG_INF("Received message: %s", rx_buffer);
+		}
 	}
 
 	(void)close(sock);
